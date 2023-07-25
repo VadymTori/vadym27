d53eaf73dc35d4bb4780c45049e17103084e0fba
Vadym <ivanovvad90@gmail.com>
2023-07-25 18:25:17 -0400

    block27
    Just started

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
 import './App.css'
+import Authenticate from "./authenticate";
+import SignUpForm from "./signupform";
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
